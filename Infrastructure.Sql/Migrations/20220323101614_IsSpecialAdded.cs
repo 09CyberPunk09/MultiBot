7f7e97159996bf1b39f48d488d435b4dd1665931
klapatyivasyl <91133723+klapatyivasyl@users.noreply.github.com>
2022-03-23 12:20:02 +0200

    Added isSpecial prop to User

---
+++ Infrastructure.Sql/Migrations/20220323101614_IsSpecialAdded.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Persistence.Sql.Migrations
+{
+    public partial class IsSpecialAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsSpecial",
+                table: "Users",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsSpecial",
+                table: "Users");
+        }
+    }
+}
