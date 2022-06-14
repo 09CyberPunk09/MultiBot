ca5c3940f932da9a9c600713dda17fe0a8abe783
maria1234564322 <glincakmaria649@gmail.com>
2022-06-14 17:16:07 +0300

    Implemented endpoints for TagsAppService

---
+++ LifeTracker.Web.Core/Models/IncomeModels/Tags/CreateTagIncomeModel.cs
@@ -0,0 +1,8 @@
+﻿namespace LifeTracker.Web.Core.Models.IncomeModels.Tags
+{
+    public class CreateTagIncomeModel
+    {
+        public string Name { get; set; }
+        public Guid UserId { get; set; }
+    }
+}
