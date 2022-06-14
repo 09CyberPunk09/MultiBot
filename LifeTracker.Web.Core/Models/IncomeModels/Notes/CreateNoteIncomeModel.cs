ec5ce14894b57c11cb73103a43d7d0ace790a686
maria1234564322 <glincakmaria649@gmail.com>
2022-06-14 15:10:02 +0300

    Implemented endpoints for NoteAppService

---
+++ LifeTracker.Web.Core/Models/IncomeModels/Notes/CreateNoteIncomeModel.cs
@@ -0,0 +1,8 @@
+﻿namespace LifeTracker.Web.Core.Models.IncomeModels.Notes
+{
+    public class CreateNoteIncomeModel
+    {
+        public string Text { get; set; }
+        public Guid UserId { get; set; }
+    }
+}
