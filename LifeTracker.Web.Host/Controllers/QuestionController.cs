393f9e66f5bf306cc9e100dcc8c51f1db5805eb4
Ostap Sikora <ostap.sikoraqwer@gmail.com>
2022-06-17 17:48:15 +0300

    Implemented endpoints based on QuestionAppService methods

---
+++ LifeTracker.Web.Host/Controllers/QuestionController.cs
@@ -0,0 +1,57 @@
+﻿using Application.Services;
+using Common.Entites;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LifeTracker.Web.Host.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class QuestionController : ControllerBase
+    {
+        private readonly QuestionAppService _service;
+
+        public QuestionController(QuestionAppService service)
+        {
+            _service = service;
+        }
+
+        [HttpPost]
+        public Question Create(Question q, Guid userId)
+        {
+          return _service.Create(q, userId);
+        }
+
+        [HttpGet]
+        public Question Get(Guid id)
+        {
+            return _service.Get(id);
+        }
+     
+        [HttpPut]
+        public Question Update(Question q)
+        {
+            return _service.Update(q);
+        }
+
+        [HttpPost]
+        public void AddSchedule(Guid questionId, string cron)
+        {
+               _service.AddSchedule(questionId, cron);
+        }
+     
+        [HttpGet]
+        public List<Question> GetQuestions(Guid userId)
+        {
+            return _service.GetQuestions(userId);
+        }
+   
+        [HttpGet]
+        public List<Question> GetScheduledQuestions()
+        {
+            return _service.GetScheduledQuestions();    
+        }
+    }
+
+}
