59377025b61965a8aafb998d5d68c658753fb9fe
klapatyivasyl <91133723+klapatyivasyl@users.noreply.github.com>
2022-07-03 15:34:10 +0300

    extended token expiration

--- LifeTracker.Web.Host/AuthConfigurer.cs
+++ LifeTracker.Web.Host/AuthConfigurer.cs
@@ -46,7 +46,7 @@ namespace LifeTracker.Web.Host
         public const string ISSUER = "MyAuthServer"; // издатель токена
         public const string AUDIENCE = "MyAuthClient"; // потребитель токена
         const string KEY = "mysupersecret_secretkey!123";   // ключ для шифрации
-        public const int LIFETIME = 1; // время жизни токена - 1 минута
+        public const int LIFETIME = 72; // время жизни токена - 1 минута
         public static SymmetricSecurityKey GetSymmetricSecurityKey()
         {
             return new SymmetricSecurityKey(Encoding.ASCII.GetBytes(KEY));
