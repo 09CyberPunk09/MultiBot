b8996065bfeff424d20cbb19046752bd1a916640
Vasyl Klapatyi <vklapatyi@gmail.com>
2022-12-18 02:32:24 +0200

    Refactored and improved configuration

---
+++ Common.Configuration/ConfigurationHelper.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Common.Configuration;
+
+public static class ConfigurationHelper
+{
+    public static IConfigurationRoot GetConfiguration()
+    {
+        var environment = Environment.GetEnvironmentVariable("ENV");
+        var confBuilder = new ConfigurationBuilder();
+        confBuilder.SetBasePath(AppContext.BaseDirectory);
+
+        //load main appsettings file
+        confBuilder.AddJsonFile($"appSettings.json");
+
+        //load environment dependent configuration
+        confBuilder.AddJsonFile($"appSettings.{environment}.json");
+        return confBuilder.Build();
+    }
+}
---
+++ Common.Configuration/ConfigurationServiceCollectionExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NLog;
+
+namespace Common.Configuration;
+
+public static class ConfigurationServiceCollectionExtensions
+{
+    private static readonly Logger logger = LogManager.GetCurrentClassLogger();
+
+    public static IServiceCollection AddConfiguration(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddSingleton(configuration);
+        logger.Info("Added configuration");
+        return services;
+    }
+
+
+    public static IServiceCollection AddSettings(this IServiceCollection services)
+    {
+
+        return services;
+    }
+}
